dd90285649ccdc7ced931e18f587ce808fc0f170
JohnParkerHodges <phodgesfl@gmail.com>
2019-10-20 22:35:54 -0400

    Spec

--- script.js
+++ script.js
@@ -8,71 +8,53 @@ const length = document.getElementById("length")
 const slider = document.getElementById("slider")
 
 function generate() {
-
     buttonClicks++;
 
-    if (!upperCaseBox.checked &&
-        !lowerCaseBox.checked &&
-        !specialCharactersBox.checked &&
-        !numbersBox.checked) {
-        display.value = 'You must choose a password type';
-        display.style.color = 'red'
-
-        if (buttonClicks > 1) {
-            boisMad()
-            setTimeout(() => display.style.backgroundColor = 'red', 50)
-            setTimeout(() => display.style.backgroundColor = 'white', 100)
-            setTimeout(() => display.style.backgroundColor = 'red', 150)
-            setTimeout(() => display.style.backgroundColor = 'white', 200)
-        }
-        return;
-    } else {
-        buttonClicks = 0;
-        display.style.color = 'black';
-        let complexity = slider.value;
-
-        //possible password characters
-        // let  values  =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_-+=";
-        let values = [];
-        const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-        const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        const special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+']
-        let password = "";
-
-
-        if (upperCaseBox.checked) {
-            values = [...values, ...upperCase];
-        }
-        if (lowerCaseBox.checked) {
-            values = [...values, ...lowerCase];
-        }
-        if (specialCharactersBox.checked) {
-            values = [...values, ...special];
-        }
-        if (numbersBox.checked) {
-            values = [...values, ...numbers];
-        }
-
-
-
-
-        //determine password length
-        for (var i = 0; i <= complexity; i++) {
-            password += values[
-                Math.floor(Math.random() * Math.floor(values.length - 1))
-            ]
-        }
-
-        //Putting password in display area
-
-        display.value = password;
+    if (vald()) {
+        makepw();
     }
 
 
 
 
 
+}
+
+function makepw() {
+    buttonClicks = 0;
+    display.value = '';
+    display.style.color = 'black';
+    let complexity = slider.value;
+
+    //possible password characters
+    // let  values  =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_-+=";
+    let values = [];
+    const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+']
+    let password = "";
+
+    if (upperCaseBox.checked) {
+        values = [...values, ...upperCase];
+    }
+    if (lowerCaseBox.checked) {
+        values = [...values, ...lowerCase];
+    }
+    if (specialCharactersBox.checked) {
+        values = [...values, ...special];
+    }
+    if (numbersBox.checked) {
+        values = [...values, ...numbers];
+    }
+    //determine password length
+    for (var i = 0; i <= complexity; i++) {
+        password += values[
+            Math.floor(Math.random() * Math.floor(values.length - 1))
+        ]
+    }
+    //Putting password in display area
+    display.value = password;
 }
 
 //setting default length
@@ -105,11 +87,31 @@ function boisMad() {
 
     if (OSName === "MacOS") {
         alert("Not Today Alex! :( You Better Give Me A Good Grade For The Read.me")
-        window.location = "https://i.imgflip.com/3dvh49.jpg"
+        window.location = "https://raw.githubusercontent.com/JohnParkerHodges/Hw03/master/y-u-no-readme.jpg"
     }
 
     if (OSName === "Windows") {
         alert("Not Today Max! :(")
-        window.location = "https://i.imgflip.com/3dvh49.jpg"
+        window.location = "https://raw.githubusercontent.com/JohnParkerHodges/Hw03/master/y-u-no-readme.jpg"
     }
+}
+
+function vald() {
+    if (!upperCaseBox.checked &&
+        !lowerCaseBox.checked &&
+        !specialCharactersBox.checked &&
+        !numbersBox.checked) {
+        display.value = 'You must choose a password type';
+        display.style.color = 'red'
+
+        if (buttonClicks > 1) {
+
+            setTimeout(() => display.style.backgroundColor = 'red', 50)
+            setTimeout(() => display.style.backgroundColor = 'white', 100)
+            setTimeout(() => display.style.backgroundColor = 'red', 150)
+            setTimeout(() => display.style.backgroundColor = 'white', 200)
+        }
+        if (buttonClicks > 3) boisMad()
+        return false;
+    } else return true;
 }
