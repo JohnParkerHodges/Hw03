7ee04552fc01b168545a4831bc829985e227eb8c
JohnParkerHodges <phodgesfl@gmail.com>
2019-10-20 21:57:07 -0400

    Added logic and fixed check boxes

--- script.js
+++ script.js
@@ -1,40 +1,95 @@
-function  generate()  {
+let buttonClicks = 0;
+const upperCaseBox = document.getElementById("upperCaseBox");
+const lowerCaseBox = document.getElementById("lowerCaseBox");
+const specialCharactersBox = document.getElementById("specialCharactersBox");
+const numbersBox = document.getElementById("numbersBox");
+const display = document.getElementById("display")
+const length = document.getElementById("length")
+const slider = document.getElementById("slider")
 
-        
-    let  complexity  =  document.getElementById("slider").value;    
+function generate() {
+    buttonClicks++;
 
-    //possible password characters
-    let  values  =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_-+=";        
-    let  password  =  "";
+    if (!upperCaseBox.checked &&
+        !lowerCaseBox.checked &&
+        !specialCharactersBox.checked &&
+        !numbersBox.checked) {
+        display.value = 'You must choose a password type';
+        display.style.color = 'red'
 
-             //determine password length
-    for  (var  i  =  0;  i  <=  complexity;  i++)  {         password  =  password  +  values.charAt(Math.floor(Math.random()  *  Math.floor(values.length  -  1)));     }
+        if (buttonClicks > 1) {
+            setTimeout(() => display.style.backgroundColor = 'red', 50)
+            setTimeout(() => display.style.backgroundColor = 'white', 100)
+            setTimeout(() => display.style.backgroundColor = 'red', 150)
+            setTimeout(() => display.style.backgroundColor = 'white', 200)
+        }
+        return;
+    } else {
+        buttonClicks = 0;
+        display.style.color = 'black';
+        let complexity = slider.value;
 
-         //Putting password in display area
-        
-    document.getElementById("display").value  =  password;
+        //possible password characters
+        // let  values  =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_-+=";
+        let values = [];
+        const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        const special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+']
+        let password = "";
+
+
+        if (upperCaseBox.checked) {
+            values = [...values, ...upperCase];
+        }
+        if (lowerCaseBox.checked) {
+            values = [...values, ...lowerCase];
+        }
+        if (specialCharactersBox.checked) {
+            values = [...values, ...special];
+        }
+        if (numbersBox.checked) {
+            values = [...values, ...numbers];
+        }
+
+
+
+
+        //determine password length
+        for (var i = 0; i <= complexity; i++) {
+            password += values[
+                Math.floor(Math.random() * Math.floor(values.length - 1))
+            ]
+        }
+
+        //Putting password in display area
+
+        display.value = password;
+    }
+
+
 
 
 
 }
 
 //setting default length
-document.getElementById("length").innerHTML = "length: 8 ";
+length.innerHTML = "length: 8 ";
 
 //function to set length based on user input
 
-document.getElementById("slider").oninput = function() {
-    if (document.getElementById("slider").value > 0) {
-        document.getElementById("length").innerHTML = "length: " + document.getElementById("slider").value;
+slider.oninput = function() {
+    if (slider.value > 0) {
+        length.innerHTML = "length: " + slider.value;
     } else {
-        document.getElementById("length").innerHTML = "length: 1"
+        length.innerHTML = "length: 1"
     }
 }
 
 //function to copy password 
 
 function copypassword() {
-    document.getElementById("display").select();
+    display.select();
     document.execCommand("Copy");
     alert("Password Copied to Clipboard");
 }
